6654f21b2d26546f880e3c5db216d35d705b298c
Karen Bolon <kbolon@c0c7c1.42berlin.de>
2024-02-19 15:10:52 +0100

    updated header and ignore file

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 15:57:05 by kbolon            #+#    #+#             */
-/*   Updated: 2024/02/05 16:03:51 by kbolon           ###   ########.fr       */
+/*   Updated: 2024/02/19 10:41:28 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 
 #include <unistd.h>
 #include <stdio.h>
+
+# define EXEC 1
+# define REDIR 2
+# define PIPE 3
+# define LIST 4
+# define BACK 5
+
+typef struct s_token
+{
+	int				*type;
+	char			*str;
+	struct s_token	*left;
+	struct s_token	*right;
+	char			char;//null character?
+}	t_token;
 
 void	makefile_check();
 
