af3491537a5b5965eb69a2ebba45cb0d05c2ef24
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2024-02-28 14:08:52 +0100

    updated header file for new structs

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 15:57:05 by kbolon            #+#    #+#             */
-/*   Updated: 2024/02/27 14:09:52 by kbolon           ###   ########.fr       */
+/*   Updated: 2024/02/28 14:08:17 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,7 @@
 
 # define EXEC 1
 # define REDIR 2
-# define PIPE 3
-# define LIST 4
+# define PIPE
 # define DELIMITER "|<>&()"
 
 typedef struct s_type
@@ -36,14 +35,14 @@ typedef struct s_type
 	int	type;
 }	t_type;
 
-typedef struct s_exec_cmd
+typedef struct s_exec
 {
 	int		type;
-	char	*argv[MAXARGS];
-	char	*arg_options[MAXARGS];
-}	t_exec_cmd;
+	char	*cmd[MAXARGS];
+	char	*options[MAXARGS];
+}	t_exec;
 
-typedef struct s_redir_cmd
+typedef struct s_redir
 {
 	int		type;
 	t_cmd	*cmd;
@@ -51,7 +50,7 @@ typedef struct s_redir_cmd
 	char	*end_file;
 	int		mode;
 	int		fd;
-}	t_redir_cmd;
+}	t_redir;
 
 typedef struct	s_cmd
 {
@@ -60,9 +59,6 @@ typedef struct	s_cmd
 	t_type	*right;
 }	t_cmd;
 
-char	*ft_strtok(char *str, char *delimiter);
-void	restore_pipes(char *str);
-void	parse_quotes_and_pipes(char *str);
-void	split_cmds(char *cmd);
+
 
 #endif
