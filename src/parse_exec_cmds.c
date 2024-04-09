1eb798b02513e04137c52bc8e7be889bfb8c289a
Karen Bolon <99751409+karenbolon@users.noreply.github.com>
2024-04-09 08:32:20 +0200

    restarting quest for memory leaks-reinstated last code that worked

--- src/parse_exec_cmds.c
+++ src/parse_exec_cmds.c
@@ -6,7 +6,7 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 14:20:46 by kbolon            #+#    #+#             */
-/*   Updated: 2024/04/08 18:06:51 by kbolon           ###   ########.fr       */
+/*   Updated: 2024/04/09 08:25:45 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,76 +25,6 @@ char	*parse_line(char *arr)
 	return (arr);
 }
 
-/*t_cmd	*init_exec_cmds(char **s, char *non_token)
-{
-	int		i;
-	int		token;
-	t_cmd	*cmd_tree;
-
-	i = 0;
-	token = 0;
-//	cmd_tree = ft_init_stuct();
-//	cmd_tree = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
-	if (!cmd_tree)
-	{
-		free(non_token);
-		return (NULL);
-	}
-//	cmd_tree = parse_for_redirections(cmd_tree, s);//check for redirs and pass the tree we have built so far
-	if (!cmd_tree)
-	{
-		free(non_token);
-		return (NULL);
-	}
-	while (*s && !is_token(**s))
-	{
-		token = find_tokens(s, &non_token);
-		if (token == 0)
-			break ;
-		cmd_tree->cmd[i] = ft_strdup(non_token);
-		if (!cmd_tree->cmd[i])
-		{
-			free_memory(s);
-			free(non_token);
-			free_cmd(cmd_tree);
-			exit (1);
-		}
-		parse_line(cmd_tree->cmd[i]);
-		i++;
-//		cmd_tree = parse_for_redirections(cmd_tree, s);
-	}
-	cmd_tree->cmd[i] = NULL;
-	return (cmd_tree);
-}
-
-t_cmd	*parse_exec_cmds(char **s)
-{
-	t_cmd	*cmd_tree;
-	char	*non_token;
-	int		i;
-
-	non_token = NULL;
-	cmd_tree = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
-	if (!cmd_tree)
-	{
-		free (non_token);
-		printf("cmd_tree initiation in exec failed\n");
-		exit (1);
-	}
-	i = 0;
-	while (i < MAXARGS)
-	{
-		cmd_tree->cmd[i] = NULL;
-		i++;
-	}
-	cmd_tree = init_exec_cmds(s, non_token);
-	if(!cmd_tree)
-	{
-		free (non_token);
-		exit (1);
-	}
-	return (cmd_tree);
-}*/
 t_cmd	*init_exec_cmds(char **s, char *non_token)
 {
 	int		i;
@@ -104,30 +34,15 @@ t_cmd	*init_exec_cmds(char **s, char *non_token)
 	i = 0;
 	token = 0;
 	cmd_tree = ft_init_stuct();
+	cmd_tree = parse_for_redirections(cmd_tree, s);//check for redirs and pass the tree we have built so far
 	if (!cmd_tree)
-	{
-		free(non_token);
 		return (NULL);
-	}
-	cmd_tree = parse_for_redirections(cmd_tree, s);
-	if (!cmd_tree)
-	{
-		free(non_token);
-		return (NULL);
-	}
 	while (*s && !is_token(**s))
 	{
 		token = find_tokens(s, &non_token);
-		if (token == 0)
+		if (token == 0)//if not a token, break
 			break ;
-		cmd_tree->cmd[i] = ft_strdup(non_token);
-		if (!cmd_tree->cmd[i])
-		{
-			free_memory(s);
-			free(non_token);
-			free_cmd(cmd_tree);
-			exit (1);
-		}
+		cmd_tree->cmd[i] = ft_strdup(non_token); 
 		parse_line(cmd_tree->cmd[i]);
 		i++;
 		cmd_tree = parse_for_redirections(cmd_tree, s);
@@ -157,8 +72,6 @@ t_cmd	*parse_exec_cmds(char **s)
 	}
 	cmd_tree = init_exec_cmds(s, non_token);
 	if(!cmd_tree)
-	{
-		exit (1);
-	}
+		free_nodes(cmd_tree);
 	return (cmd_tree);
 }
--- src/utils.c
+++ src/utils.c
@@ -6,13 +6,49 @@
 /*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 14:53:32 by kbolon            #+#    #+#             */
-/*   Updated: 2024/04/08 18:00:20 by kbolon           ###   ########.fr       */
+/*   Updated: 2024/04/09 08:29:32 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	free_cmd(t_cmd *cmd)
+
+void	free_cmdtree(t_cmd *tree)
+{
+//	int	i;
+
+//	i = 0;
+	if (!tree)
+		return ;
+	free_nodes(tree);
+	if (tree->file_name)
+		free(tree->file_name);
+//	if (tree->m_env)
+//		free_nodes(tree->m_env);
+}
+
+void	free_nodes(t_cmd *node)
+{
+	t_cmd	*temp;
+
+	if (!node)
+		return ;
+	while (node)
+	{
+		temp = node;
+		if (temp->fd_in)
+			close(temp->fd_in);
+		if (temp->fd_out)
+			close(temp->fd_out);
+		if (temp->m_env)
+			free(temp->m_env->cmd_env);
+		free(temp->file_name);
+		node = node->next;
+		free(temp);
+	}
+}
+
+/*void	free_cmd(t_cmd *cmd)
 {
 	if (!cmd)
 		return ;
@@ -29,7 +65,7 @@ void	free_cmd(t_cmd *cmd)
 		cmd = cmd->next;
 	}
 	free (cmd);
-}
+}*/
 
 void	free_memory(char **arr)
 {
