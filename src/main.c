89f89f6d28e926259b39cb20ac8674d85e79d806
Karen Bolon <kbolon@c4b11c5.42berlin.de>
2024-05-02 19:52:03 +0200

    sort of working with core dumps with exp

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chbuerge <chbuerge@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kbolon <kbolon@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 15:54:42 by kbolon            #+#    #+#             */
-/*   Updated: 2024/05/02 17:25:27 by chbuerge         ###   ########.fr       */
+/*   Updated: 2024/05/02 19:39:04 by kbolon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,13 +105,12 @@ int	main(int argc, char **argv, char **env)
 		}
 //		printf("line to be parsed: %s\n", line);
 		add_history(line);
-		if (check_for_var(&line, &exp) == 1)
+		check_for_var(&line, &exp);
+		printf("string after check_for_var: %s\n", line);
+		printf("string len: %lu\n", strlen(line));
+		if (strlen(line) > 0)
 		{
-			print_exp(exp);
-			break ;
-		}
-		if (line)
-		{
+			printf("command found\n");
 			parse_for_cmds(&list, line);//need to add envp
 //			parse list for expansions???
 			print_stack(list);
@@ -121,6 +120,8 @@ int	main(int argc, char **argv, char **env)
 				return (0);
 			}
 		}
+		if (exp)
+			print_exp(exp);
 		ft_executor(list, env_list);
 		dup2(original_stdin, STDIN_FILENO);
 		// close(original_stdin);
@@ -137,3 +138,56 @@ int	main(int argc, char **argv, char **env)
 	free_exp(exp);
 	return (0);
 }
+/*
+int	main(int argc, char **argv, char **env)
+{
+	static char	*line;
+//	char	*line = " grep lady < infile.txt | nl";
+	t_cmd	*list;
+	t_env	*env_list;
+//	int exit_status = 0;
+
+	(void)argc;
+	(void)argv;
+	list = NULL;*/
+/*	list = ft_init_struct();
+	if (!list)
+	{
+		free(line);
+		return (0);
+	}*/
+/*	if (argc != 1)
+	{
+		write(STDERR_FILENO, "invalid arguments: ambiguous redirect\n", 38);
+		exit(1);//is this the right error code?
+	}
+	if (!env)
+		return (0);
+	env_list = fill_env_struct(env);
+	while (1)
+	{
+		line = readline("minishell: ");
+		if (!line)
+		{
+			printf("Problems reading input");
+			exit(1);
+		}
+		printf("line to be parsed: %s\n", line);
+//		add_history(line);
+		parse_for_cmds(&list, line);//need to add envp
+		print_stack(list);
+		if (!list)
+		{
+			free(line);
+			return (0);
+		}
+		ft_executor(list, env_list);
+		list = NULL; // here needs to be freed
+		// free(line);
+		// free_cmdtree(list);
+	}
+//	exit_status = ft_executor(list);
+//	free(line);
+//	free_cmdtree(list);
+//	return (exit_status);
+}*/
