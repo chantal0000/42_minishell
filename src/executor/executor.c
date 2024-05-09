9a9d67e387d4f3c935244d3980e2d591ba96f8c2
Chantal Munja Buerger <c_buerger@gmx.net>
2024-05-09 12:52:40 +0200

    norminette executor.c of functions: ft_executor, loops_cmds

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: chbuerge <chbuerge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 11:35:42 by chbuerge          #+#    #+#             */
-/*   Updated: 2024/05/08 18:41:51 by chbuerge         ###   ########.fr       */
+/*   Updated: 2024/05/09 12:51:11 by chbuerge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,7 +188,6 @@ int	ft_pipe_last(t_cmd *node, int pipe_fd[2], int old_pipe_in, t_env *env_list)
 			exit (exit_status);
 		if (execute_cmd(ft_env_list_to_array(env_list), node->cmd) == 127)
 		{
-			printf("HI\n\n");
 			ft_free_env_list(env_list);
 			ft_free_cmd_struct(first_node(node));
 			exit (127);
@@ -209,43 +208,88 @@ int	ft_pipe_last(t_cmd *node, int pipe_fd[2], int old_pipe_in, t_env *env_list)
 ** 6. we save the old fd_in into old_pipe_in
 ** 7. move on to the next node in the linked list
 */
+// void ft_execute_pipeline_cmd(t_cmd *node, int *old_pipe_in, t_env *env_list)
+// {
+// 	int	pipe_fd[2];
+// 	if (node->next && node->prev)
+// 	{
+// 		pipe(pipe_fd);
+// 		ft_pipe_middle(node, pipe_fd, old_pipe_in, env_list);
+// 	}
+// 	else if (node->next)
+// 	{
+// 		pipe(pipe_fd);
+// 		ft_pipe_first(node, pipe_fd, env_list);
+// 	}
+// 	else
+// 		ft_pipe_last(node, pipe_fd, *old_pipe_in, env_list);
+// 	*old_pipe_in = pipe_fd[0];
+// }
 
-int	loop_cmds(t_cmd *node, int exit_status, t_cmd *head, t_env *env_list)
+void	ft_reset_std(int std_in, int std_out)
 {
-	int old_pipe_in = 0;
-	int pipe_fd[2];
-	int std_in = dup(STDIN_FILENO);
-	int std_out = dup(STDOUT_FILENO);
-	while(node)
-	{
-		if (node->next && node->prev)
-		{
-			// printf("entering middle pipe cmd is %s\n", node->cmd[0]);
-			write(2, "\nentering middle pipe\n", 22);
-			pipe(pipe_fd);
-			ft_pipe_middle(node, pipe_fd, old_pipe_in, env_list);
-		}
-		else if(node->next)
-		{
-			// write(2, "\nentering first pipe\n", 21);
-			pipe(pipe_fd);
-			ft_pipe_first(node, pipe_fd, env_list);
-			// printf("entering first pipe cmd is %s\n", node->cmd[0]);
-		}
-		else
-		{
-			// write(2, "\nentering last pipe\n", 21);
-			ft_pipe_last(node, pipe_fd, old_pipe_in, env_list);
-			// printf("entering last pipe, cmd is %s\n", node->cmd[0]);
-		}
-		old_pipe_in = pipe_fd[0];
 		dup2(std_in, STDIN_FILENO);
 		dup2(std_out, STDOUT_FILENO);
+}
+
+// int	loop_cmds(t_cmd *node, t_cmd *head, t_env *env_list)
+// {
+// 	int	old_pipe_in;
+// 	int	pipe_fd[2];
+// 	int	std_in;
+// 	int	std_out;
+
+// 	old_pipe_in = 0;
+// 	std_in = dup(STDIN_FILENO);
+// 	std_out = dup(STDOUT_FILENO);
+// 	while (node)
+// 	{
+// 		if (node->next && node->prev)
+// 		{
+// 			pipe(pipe_fd);
+// 			ft_pipe_middle(node, pipe_fd, old_pipe_in, env_list);
+// 		}
+// 		else if (node->next)
+// 		{
+// 			pipe(pipe_fd);
+// 			ft_pipe_first(node, pipe_fd, env_list);
+// 		}
+// 		else
+// 			ft_pipe_last(node, pipe_fd, old_pipe_in, env_list);
+// 		old_pipe_in = pipe_fd[0];
+// 		ft_reset_std(std_in, std_out);
+// 		node = node->next;
+// 	}
+// 	close_after(std_in, std_out, pipe_fd);
+// 	return (handle_exit_status(head));
+// }
+
+int	loop_cmds(t_cmd *node, t_cmd *head, t_env *env_list)
+{
+	int	old_pipe_in;
+	int	pipe_fd[2];
+	int	std_in;
+	int	std_out;
+
+	old_pipe_in = 0;
+	std_in = dup(STDIN_FILENO);
+	std_out = dup(STDOUT_FILENO);
+	while (node)
+	{
+		if (node->next)
+			pipe(pipe_fd);
+		if (!node->prev && node->next)
+			ft_pipe_first(node, pipe_fd, env_list);
+		else if (node->prev && node->next)
+			ft_pipe_middle(node, pipe_fd, old_pipe_in, env_list);
+		else
+			ft_pipe_last(node, pipe_fd, old_pipe_in, env_list);
+		old_pipe_in = pipe_fd[0];
+		ft_reset_std(std_in, std_out);
 		node = node->next;
 	}
 	close_after(std_in, std_out, pipe_fd);
-	exit_status = handle_exit_status(head);
-	return(exit_status);
+	return (handle_exit_status(head));
 }
 
 void	close_after(int std_in, int std_out, int pipe_fd[2])
@@ -275,14 +319,11 @@ int	ft_executor(t_cmd *node, t_env *env_list)
 	exit_status = 0;
 	head = node;
 	pid = 0;
-
 	ft_init_signals_input();
 	if (!node->next && !node->prev)
 		exit_status = ft_simple_cmd(node, exit_status, pid, env_list);
 	else
-		exit_status = loop_cmds(node, exit_status, head, env_list);
-
-	printf("freeing\n");
+		exit_status = loop_cmds(node, head, env_list);
 	ft_free_cmd_struct(node);
 	return (exit_status);
 }
