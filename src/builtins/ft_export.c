3b902bd56df8ee83fca2131122d0f7e964385409
Chantal Munja Buerger <c_buerger@gmx.net>
2024-05-13 10:09:56 +0200

    ft_export norminette & error msg

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: chbuerge <chbuerge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 11:16:16 by chbuerge          #+#    #+#             */
-/*   Updated: 2024/05/12 15:31:36 by chbuerge         ###   ########.fr       */
+/*   Updated: 2024/05/13 10:06:00 by chbuerge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,56 @@
 /*
 ** export with no options
 ** sets environment variables
-** if used without arguments it prints all env variables (env?)
-** its possible to add multiple var to env
-
+** if used without arguments it prints all env variables
+// bash: export: `var@=hello': not a valid identifier
 */
 
-// case1 no args, print env with declare -x
-// case2 witha args
-	//check if valid
-	//check if it already exits?
-		//if yes update
-	//else create
-// char	create_str(char *str)
-// bash: export: `var@=hello': not a valid identifier
+/*
+** checks that there is at least one equal sign otherwise returns 1 to show err
+*/
+int	check_for_equal(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '=')
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 int	ft_check_syntax(char *str)
 {
 	int	i;
-	int	flag_equal = 0;
 
 	i = 0;
-	while(str[i] && str[i] != '=')
+	while (str[i] && str[i] != '=')
 	{
-		if (str[0] != '_' && (ft_isalpha(str[0]) != 1))
+		if ((str[0] != '_') && (ft_isalpha(str[0]) != 1))
 		{
-			printf("str[0] is %c return -1\n", str[0]);
+			printf("%s: not a valid identifier\n", str);
 			return (1);
 		}
-		if(str[i] != '_' && (ft_isalnum(str[i]) != 1))
+		if ((str[i] != '_') && (ft_isalnum(str[i]) != 1))
 		{
-			printf("before '=' %c\n", str[i]);
+			printf("%s: not a valid identifier\n", str);
 			return (1);
 		}
 		i++;
 	}
-	while (str[i])
-	{
-		if (str[i] == '=')
-			flag_equal = 1;
-		i++;
-	}
-		i++;
-	if (flag_equal != 1)
+	if (check_for_equal(str) == 1)
 		return (1);
 	return (0);
 }
+
 int	ft_export(t_cmd *cmd, t_env **env_list)
 {
-	t_env *temp = *env_list;
+	t_env	*temp;
+
+	temp = *env_list;
 	if (cmd->cmd[1] == NULL)
 	{
 		while ((temp != NULL) && (temp->cmd_env != NULL))
@@ -80,5 +82,4 @@ int	ft_export(t_cmd *cmd, t_env **env_list)
 		return (0);
 	}
 	return (1);
-
 }
